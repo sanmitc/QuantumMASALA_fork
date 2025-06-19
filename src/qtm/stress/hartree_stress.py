0a1cccbfb7c8a06908a79826e598767488af19b9
sanmitc <sanmitc@iisc.ac.in>
2025-06-19 16:03:42 +0530

    Removed loops to optimize Hartree contribution to stress

--- src/qtm/stress/hartree_stress.py
+++ src/qtm/stress/hartree_stress.py
@@ -9,20 +9,24 @@ def stress_har(rho:FieldGType,
                gamma_only:bool=False):
 
     ## Extracting the g space characteristics
-    gnum=gspc.size_g
-    cart_g=(gspc.g_cart.T[gspc.idxsort]).T
+    g_cart=gspc.g_cart
     norm2=gspc.g_norm2[gspc.idxsort]
+    mask=norm2>1e-10
+    norm2=norm2[mask]
+    gnum=len(norm2)
+    cart_g=g_cart[:,gspc.idxsort]
+    cart_g=cart_g[:,mask]
     rho=rho._data[0, rho.gspc.idxsort]/np.prod(rho.gspc.grid_shape)
+    rho=rho[mask]
 
     ##Constants
     _4pi=4*PI*ELECTRON_RYD**2
 
     stress_har=np.zeros((3,3))
-    for ig in range(1,gnum):
-        gvec=cart_g[:, ig].reshape(3,-1)
-        gmatrix=2*gvec@gvec.T/norm2[ig]-np.eye(3)
-        gmatrix*=np.abs(rho[ig])**2/norm2[ig]
-        stress_har+=gmatrix
+    g_tensor=2*np.einsum("ij, ik->ijk", cart_g.T, cart_g.T)/(norm2.reshape(-1,1,1))-[np.eye(3)]*gnum
+    g_tensor*=((np.abs(rho)**2/norm2).reshape(-1,1,1))
+    stress_har=np.sum(g_tensor, axis=0)
+
     fac=1 if gamma_only else 0.5
     stress_har*=-_4pi*fac
     return stress_har*RY_KBAR
