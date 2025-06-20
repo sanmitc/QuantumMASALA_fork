33818806268c3374b9bf4ebf9a541ab2f18de70d
sanmitc <sanmitc@iisc.ac.in>
2025-06-20 17:03:39 +0530

    GSpace Parallelization in calculation of Ewald Stress

--- src/qtm/stress/ewald_stress.py
+++ src/qtm/stress/ewald_stress.py
@@ -5,55 +5,9 @@ from qtm.lattice import ReciLattice
 from qtm.crystal import Crystal
 from qtm.gspace import GSpace
 from qtm.constants import ELECTRON_RYD, PI, RY_KBAR
+from qtm.dft import DFTCommMod
 
 EWALD_ERR_THR = 1e-7  # TODO: In quantum masala ewald energy code it is set to 1e-7
-
-
-def rgen2(rmax: float,
-         max_num: int,
-         beta: float,
-         latvec: np.ndarray,
-         dtau: np.ndarray):
-    """r max: the maximum radius we take into account
-
-
-    max_num: maximum number of r vectors
-
-
-    latvec: lattice vectors, each column representing a vector.
-            Numpy array with dimensions (3,3)
-
-    recvec: reciprocal lattice vectors, each column representing a vector.
-            Numpy array with dimensions (3,3)
-
-    dtau:difference between atomic positions. numpy array with shape (3,)"""
-
-    if rmax == 0:
-        raise ValueError("rmax is 0, grid is non-existent.")
-    # making the grid
-    n = np.floor(4 / beta / np.linalg.norm(latvec, axis=1)).astype('i8') + 2
-    ni = n[0]
-    nj = n[1]
-    nk = n[2]
-
-    vec_num = 0
-    r = np.zeros((3, max_num))
-    r_norm = np.zeros((max_num))
-    for i in range(-ni, ni):
-        for j in range(-nj, nj):
-            for k in range(-nk, nk):
-                t = i * latvec[:, 0] + j * latvec[:, 1] + k * latvec[:, 2] - dtau
-                t_norm = np.linalg.norm(t)
-                if t_norm < rmax and np.abs(t_norm) > 1e-5:
-                    r[:, vec_num] = t
-                    r_norm[vec_num] = t_norm
-                    vec_num += 1
-
-                if vec_num >= max_num:
-                    raise ValueError(f"maximum allowed value of r vectors are {max_num}, got {vec_num}. ")
-    return r, r_norm, vec_num
-
-
 
 def transgen(latvec: np.ndarray,
          rmax: float):
@@ -112,6 +66,7 @@ def rgen(trans:np.ndarray,
 
 
 def stress_ewald(
+        dftcomm: DFTCommMod,
         crystal: Crystal,
         gspc: GSpace,
         gamma_only: bool = False) -> np.ndarray:
@@ -132,13 +87,21 @@ def stress_ewald(
         Primvec of Gspc.recilat: The columns represent the reciprocal lattice vectors"""
     # getting the characteristic of the g_vectors:
 
-    idxsort=gspc.idxsort
-    gvec_nonzero = gspc.g_cryst[:, 1:]
-    gvec_nonzero=(gvec_nonzero.T[idxsort[1:]-1]).T
+    '''idxsort=gspc.idxsort
     gcart_nonzero = gspc.g_cart[:, 1:]
     gcart_nonzero = (gcart_nonzero.T[idxsort[1:] - 1]).T
-    gg_nonzero = np.sum(gcart_nonzero * gcart_nonzero, axis=0)
-    gnum=gspc.size_g-1
+    gg_nonzero = np.sum(gcart_nonzero * gcart_nonzero, axis=0)'''
+
+    norm2=gspc.g_norm2
+    mask=norm2>1e-10
+    gg_nonzero=norm2[mask]
+    gcart_nonzero=gspc.g_cart[:,mask]
+
+
+    '''gcart_nonzero = gspc.g_cart.T[np.linalg.norm(gspc.g_cart.T, axis=1) > 1e-5]
+    gg_nonzero = np.linalg.norm(gcart_nonzero, axis=1)**2
+    gcart_nonzero = gcart_nonzero.T
+    print("gg", dftcomm.pwgrp_intra.rank, "=", gg_nonzero)'''
 
     # getting the crystal characteristics
     l_atoms = crystal.l_atoms
@@ -156,7 +119,7 @@ def stress_ewald(
 
     # calculating the analogous structure factor:
     str_fac = np.sum(np.exp(1j * coords_cart_all.T @ gcart_nonzero) * valence_all.reshape(-1, 1), axis=0)
-
+    print("str_fac_shape", dftcomm.pwgrp_intra.rank , "=", str_fac.shape)
     # getting the error bounds TODO: write the error formula
     alpha = 2.8
 
@@ -183,14 +146,17 @@ def stress_ewald(
     g_tensor=2*np.einsum('ij, ik->ijk', gcart_nonzero.T, gcart_nonzero.T)
     g_tensor*=((gg_nonzero/(4*alpha)+1)/gg_nonzero*sewald).reshape(-1,1,1)
     S_L=-np.sum(g_tensor, axis=0)
+    if dftcomm.pwgrp_intra!=None: S_L=dftcomm.pwgrp_intra.allreduce(S_L)
 
     total_charge=np.sum(valence_all)
     s_self= PI/(2*alpha)*ELECTRON_RYD**2*(total_charge/omega)**2
 
     sewald = np.sum(sewald)
+    if dftcomm.pwgrp_intra!=None: sewald=dftcomm.pwgrp_intra.allreduce(sewald)
     s_self=s_self-sewald
     for idx in range(3):
         S_L[idx,idx]-=s_self
+    
 
     rmax = 5 / eta / alat
     max_num = 100
