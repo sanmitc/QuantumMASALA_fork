Added and fixed parallelization in stress calculations. 1. In kinetic stress, fixed a bug regarding slicing the band index. 2. In Hartree stress, added parallelization for the calculation of the stress tensor. 3. Minor cleanup for Ewald stress. 4. For local stress, fixed parallelization bug regarding sorting the g vectors. 5. Loc.py had minor bug fixes.

src/qtm/stress/kinetic_stress.py
@@ -7,10 +7,15 @@ from qtm.mpi import scatter_slice
 
 __all__=["kinetic_stress"]
 
-def stress_kinetic(dftcomm:DFTCommMod, numbnd:int, cryst:Crystal, wfn_k_group:tuple):
+def stress_kinetic(dftcomm:DFTCommMod, 
+                   numbnd:int, 
+                   cryst:Crystal, 
+                   wfn_k_group:tuple):
     with dftcomm.kgrp_intra as comm:
-        band_slice=scatter_slice(numbnd, comm.size, comm.rank)
-
+        if dftcomm.pwgrp_intra is None:
+            band_slice = scatter_slice(numbnd, comm.size, comm.rank)
+        else:
+            band_slice = np.arange(numbnd)
     omega=cryst.reallat.cellvol
     me_HART=1.
     hcut_HART=1.
@@ -19,16 +24,18 @@ def stress_kinetic(dftcomm:DFTCommMod, numbnd:int, cryst:Crystal, wfn_k_group:tu
         for wfn in k_wfn:
             gkcart=wfn.gkspc.gk_cart.T
             occ_num=wfn.occ[band_slice] #Occupation numbers
-
             k_weight=wfn.k_weight
             evc_gk=wfn.evc_gk[band_slice]
             quant = np.sum(np.abs(evc_gk.data) ** 2*occ_num.reshape(-1,1), axis=0)
             gk_tensor=np.einsum("ij, ik->ijk", gkcart, gkcart)  
             gk_tensor*=quant.reshape(-1,1,1)
-            kin_stress+=np.sum(gk_tensor, axis=0)*k_weight
+            kin_stress_k=np.sum(gk_tensor, axis=0)*k_weight
+            kin_stress+=kin_stress_k
     kin_stress*=ELECTRON_RYD**2*hcut_HART**2/me_HART/omega
-
-    kin_stress=cryst.symm.symmetrize_matrix(kin_stress)
-    with dftcomm.image_comm as comm:
-        comm.Allreduce(comm.IN_PLACE, kin_stress)
+    '''if dftcomm.kgrp_intra is not None:
+        kin_stress = dftcomm.kgrp_intra.allreduce(kin_stress)'''
+    #kin_stress=cryst.symm.symmetrize_matrix(kin_stress)
+    if dftcomm.image_comm is not None:
+        #print("kinetic stress in", dftcomm.image_comm.rank, "=", kin_stress)
+        kin_stress = dftcomm.image_comm.allreduce(kin_stress)
     return kin_stress*RY_KBAR